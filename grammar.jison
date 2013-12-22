722c734ffac041543b1f4f0f643e95b76406dbe6
Tawan Sierek <tawan.sierek@gmail.com>
2013-12-22 18:30:16 +0100

    while (x == 5) {}

--- grammar.jison
+++ grammar.jison
@@ -7,6 +7,15 @@
 \s+                   /* skip whitespace */
 ";"                   return 'SEMI'
 [0-9]+("."[0-9]+)?\b  return 'NUMBER'
+"while"               return 'WHILE'
+"true"                return 'TRUE'
+"false"               return 'FALSE'
+"=="                  return 'EQUALS'
+"!="                  return 'ISNOT'
+"{"                   return '{'
+"}"                   return '}'
+">"                   return 'GRT'
+"<"                   return 'LGT'
 [a-z]+                return 'IDENT'
 "*"                   return '*'
 "="                   return '='
@@ -26,18 +35,42 @@ expressions
         { return SimpleScript.create($1); }
     ;
 
+block
+    : '{' stmt_list '}'
+        { $$ = $2 ; }
+    ;
+
+condition
+    : exp EQUALS exp
+        { $$ = SimpleScript.equals($1, $3); }
+    | exp ISNOT exp
+        { $$ = SimpleScript.isnot($1, $3); }
+    | exp GRT exp
+        { $$ = SimpleScript.greater($1, $3); }
+    | exp LGT exp
+        { $$ = SimpleScript.lower($1, $3); }
+    | TRUE
+        { $$ = SimpleScript.boolean(true); }
+    | FALSE
+        { $$ = SimpleScript.boolean(false); }
+    ;
+
 stmt_list
     : stmt SEMI
         { $$ = [ $1 ]; }
     | stmt_list stmt SEMI
         { $1.push($2); $$ = $1; }
     ;
+
 stmt
     : IDENT '=' exp
         { $$ = SimpleScript.assignment($1, $3); }
     | exp
         { $$ = $1; }
+    | WHILE condition block
+        { $$ = SimpleScript.while_loop($2, $3) ;}
     ;
+
 exp
     : exp '*' exp
         { $$ = SimpleScript.multiply($1, $3); }
@@ -46,4 +79,3 @@ exp
     | IDENT
         { $$ = SimpleScript.ident($1);}
     ;
-
--- simpleScript.js
+++ simpleScript.js
@@ -81,6 +81,63 @@ SimpleScript = {
     };
   },
 
+  equals: function(left, right) {
+    return {
+      exec: function(q) {
+        var val = left.exec(q) == right.exec(q);
+        var that = this;
+        q.push(function() { that.el.toggle(); that.el.toggle("puff");});
+        q.push(function() { that.el.html(val ? "true" : "false"); });
+        return val ;
+      },
+
+      output: function() {
+        this.el = jQuery(document.createElement("div"));
+        this.el.append(left.output());
+        this.star = jQuery(document.createElement("div"));
+        this.star.html("==");
+        this.el.append(this.star);
+        this.el.append(right.output());
+        return this.el;
+      }
+    };
+  },
+
+  while_loop: function(cond, block) {
+    return {
+      exec: function(q) {
+        while(cond.exec(q)) {
+          jQuery.each(block, function(i, a) {
+            a.exec(q);
+          });
+        }
+      },
+
+      output: function() {
+        this.el = jQuery(document.createElement("div"));
+        this.while_el = jQuery(document.createElement("div"));
+        this.while_el.html("while ");
+        this.el.append(this.while_el);
+        this.el.append(cond.output());
+        var open_curl = jQuery(document.createElement("div"));
+        open_curl.html("{");
+        this.el.append(open_curl);
+        var that = this;
+        jQuery.each(block, function(i, stmt) {
+          var div = jQuery(document.createElement("div"));
+          div.addClass("no-inline");
+          div.append(stmt.output());
+          that.el.append(div);
+        });
+        var close_curl = jQuery(document.createElement("div"));
+        close_curl.addClass("no-inline");
+        close_curl.html("}");
+        this.el.append(close_curl);
+        return this.el;
+      }
+    }
+  },
+
   heap: {},
 
   output: function(script, element) {
