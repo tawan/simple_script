7e8ce726dcc22b3a181807e0e898d97ea022ec31
Tawan Sierek <tawan.sierek@gmail.com>
2014-01-16 16:57:58 +0100

    Decorate arrays with each method.

--- src/Enumerable.js
+++ src/Enumerable.js
@@ -1,13 +1,19 @@
 var SimpleScript = (function(my) {
   var enumerable = Object.create(Array.prototype);
-  enumerable.each = function(fn) {
+  var iterator = function(fn) {
     for (var i = 0; i < this.length; i++) {
       fn(this[i]);
     }
   };
 
-  my.createEnumerable = function() {
-    return Object.create(enumerable);
+  enumerable.each = iterator;
+  my.createEnumerable = function(arr) {
+    if (typeof arr == "undefined") {
+      return Object.create(enumerable);
+    }
+    else {
+      arr.each = iterator;
+    }
   };
 
   return my;
